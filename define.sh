2dd12abef8b4ea28c445daf2b9b9da52e931d3e5
Jack Zampolin <jack.zampolin@gmail.com>
2017-05-23 17:25:13 -0700

    Seperate out into diff folders, generalize

--- define.sh
+++ define.sh
@@ -2,16 +2,20 @@
 
 set -e
 
-kapacitor define-template normalize-node_exporter -type stream -tick normalize-node_exporter.tmpl.tick
-kapacitor define node_exporter-influxdb-out -type stream -tick influxdb_out.tick -dbrp prometheus.autogen
-kapacitor enable node_exporter-influxdb-out
+source=$1
 
-for f in measurements/*.json
+[ -z "$source" ] && echo "Need to pass source as arg..." && exit 1;
+
+kapacitor define-template normalize-prometheus -type stream -tick normalize-prometheus.tmpl.tick
+kapacitor define prometheus-out -type stream -tick prometheus-out.tick -dbrp prometheus.autogen
+kapacitor enable prometheus-out
+
+for f in $source/*.json
 do
     name=$(basename $f)
     name=${name/.json/}
     kapacitor define normalize-$name \
-        -template normalize-node_exporter \
+        -template normalize-prometheus \
         -dbrp prometheus_raw.autogen \
         -vars $f
     kapacitor enable normalize-$name
